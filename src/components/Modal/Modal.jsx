ad43b6c86e1811387c20bfb90a9925c2cb6d845c
Роман Хоружий <roman.khoruzhy@gmail.com>
2023-11-14 06:28:34 +0300

    Создать портал для модального окна

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -2,22 +2,34 @@ import React from 'react';
 import style from './Modal.module.css';
 import {ReactComponent as CloseIcon} from './img/close.svg';
 import PropTypes from 'prop-types';
+import Markdown from 'markdown-to-jsx';
+import ReactDOM from 'react-dom';
 
-export const Modal = ({title, author, markdown}) => {
-  console.log(markdown);
-  return (
-    <div className={style.overlay}>
-      <div className={style.modal}>
-        <h2 className={style.title}>{title}</h2>
-        <div className={style.content}>{markdown}</div>
-        <p className={style.author}>{author}</p>
-        <button className={style.button}>
-          <CloseIcon />
-        </button>
+export const Modal = ({title, author, markdown}) => ReactDOM.createPortal(
+  <div className={style.overlay}>
+    <div className={style.modal}>
+      <h2 className={style.title}>{title}</h2>
+      <div className={style.content}>
+        <Markdown options={{
+          overrides: {
+            a: {
+              props: {
+                target: '_blank',
+              },
+            },
+          },
+        }}>
+          {markdown}
+        </Markdown>
       </div>
+      <p className={style.author}>{author}</p>
+      <button className={style.button}>
+        <CloseIcon />
+      </button>
     </div>
-  );
-};
+  </div>,
+  document.getElementById('modal-root'),
+);
 
 Modal.propTypes = {
   author: PropTypes.string,

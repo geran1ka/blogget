4fe9835939ee5712e959c4d3c0605a11c5d104ad
Geran1ka <roman.khoruzhy@gmail.com>
2023-12-26 15:45:52 +0300

    redux-saga

--- src/store/index.js
+++ src/store/index.js
@@ -4,6 +4,13 @@ import postsReducer from './posts/postsSlice';
 import commentsReducer from './comments/commentsSlice';
 import {configureStore} from '@reduxjs/toolkit';
 import {commentReducer} from './comment/commentReducer';
+import {searchReducer} from './search/searchReducer';
+import createSagaMiddleware from 'redux-saga';
+import rootSaga from './saga';
+
+
+const sagaMiddleware = createSagaMiddleware();
+
 
 export const store = configureStore({
   reducer: {
@@ -12,7 +19,9 @@ export const store = configureStore({
     auth: authReducer,
     posts: postsReducer,
     comments: commentsReducer,
+    search: searchReducer,
   },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(tokenMiddleware),
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(tokenMiddleware, sagaMiddleware),
 });
 
+sagaMiddleware.run(rootSaga);
---
+++ src/store/search/searchSaga.js
@@ -0,0 +1,25 @@
+import {takeLatest, put, select, call} from 'redux-saga/effects';
+import {URL_API} from '../../api/const';
+import axios from 'axios';
+
+import {SEARCH_REQUEST, searchRequestError, searchRequestSuccess} from './searchAction';
+
+function* fetchSearch(action) {
+  const token = yield select(state => state.token.token);
+
+  try {
+    const request = yield call(axios, `${URL_API}/search?q=${action.search}`, {
+      headers: {
+        Authorization: `bearer ${token}`,
+      },
+    });
+
+    yield put(searchRequestSuccess(request.data.data));
+  } catch (error) {
+    yield put(searchRequestError(error));
+  }
+}
+
+export function* watchSearch() {
+  yield takeLatest(SEARCH_REQUEST, fetchSearch);
+}
